21e320a07a5ae717830252841d35c33c700ef2d3
Adarsh Rawal <adarshrawal321@gmail.com>
2025-12-26 14:53:44 +0545

    Project Started

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req, res) => {
+  res.send("Status Okay!!");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
---
+++ backend/models/userModel.js
@@ -0,0 +1,41 @@
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+
+    avatar: {
+      type: String,
+      default: "",
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
